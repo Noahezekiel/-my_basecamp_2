695d175f648f16f189d1bf023b1c8e444db33e00
quit <nnooaahh24@gmail.com>
2025-04-20 23:00:37 +0100

    Clean up importmap pins

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,12 @@
-# Pin npm packages by running ./bin/importmap
+pin "application", preload: true
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
+# Stimulus
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "application", preload: true
-pin "bootstrap", to: "bootstrap.bundle.min.js"
+
+# Turbo
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+
+# Bootstrap (use CDN-hosted version)
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
