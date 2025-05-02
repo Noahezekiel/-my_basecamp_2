1955de866c2158ff90bb167febebdd3a82f48273
quit <nnooaahh24@gmail.com>
2025-05-02 09:50:06 +0100

    Login alart

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,12 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-
+  
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      puts "Session set: #{session[:user_id]}"
       redirect_to dashboard_path
     else
-      flash[:alert] = "Invalid email or password"
+      flash.now[:alert] = "Invalid email or password"
       render :new
     end
   end
